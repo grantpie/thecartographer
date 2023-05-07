fbe0fd9178872c6cef97728ec96c192d4f82a2e9
grantpie <grant.public1@gmail.com>
2023-05-07 16:58:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: 'https://grantpie.github.io/thecartographer/' ,
+    base: '/thecartographer/' ,
   plugins: [vue()],
   resolve: {
     alias: {
