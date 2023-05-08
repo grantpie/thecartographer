bca9da6ec370745f93c25d4981cc72dacedbf06f
grantpie <grant.public1@gmail.com>
2023-05-08 08:19:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from '../node_modules/vue';
+import Vue from 'vue';
 import App from './App.vue'
 import router from './router'
 
 //bootstrap 5 and base styles
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import './assets/scss/base.scss'
 
-import '../node_modules/bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/js/bootstrap.js'
 const app = createApp(App)
 
 app.use(router)
